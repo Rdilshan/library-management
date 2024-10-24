fefa77fa6ef229bdda5c97673a391ecbf00bf0f7
Randika Dilshan <rdilshan077788@gmail.com>
2024-10-25 02:30:22 +0530

    Admin CRUD + jwt token ♨

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,22 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import {Adminresolver} from "./graphql/resolvers/Adminresolver";
+import { Adminresolver } from './graphql/resolvers/Adminresolver';
 import { PrismaService } from './prisma.service';
+import { JwtModule } from '@nestjs/jwt';
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
-      autoSchemaFile:"./src/schema.gql"
+      autoSchemaFile: './src/schema.gql',
     }),
-    
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '2h' },
+    }),
   ],
   controllers: [],
-  providers: [PrismaService,Adminresolver],
+  providers: [PrismaService, Adminresolver],
 })
-
-export class AppModule  {}
+export class AppModule {}
--- src/graphql/resolvers/Adminresolver.ts
+++ src/graphql/resolvers/Adminresolver.ts
@@ -1,25 +1,85 @@
-import { Resolver, Query, Args, Int } from '@nestjs/graphql';
-import { Admin } from '../model/admin';
+import { Resolver, Query, Args, Int, Mutation } from '@nestjs/graphql';
+import { Admin, Role } from '../model/admin';
 import { PrismaService } from '../../prisma.service';
+import * as bcrypt from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
+
 
 export
 @Resolver()
 class Adminresolver {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    private jwtService: JwtService,
+  ) {}
 
   @Query(() => [Admin])
   async getAdmins() {
     return this.prisma.admin.findMany();
   }
-  
+
   @Query(() => Admin)
   async getAdmin(@Args('id', { type: () => Int }) id: number) {
-    id=Number(id)
+    id = Number(id);
     return this.prisma.admin.findUnique({ where: { id } });
   }
 
+  @Query()
+  async LoginAdmin(@Args('email') email: string, @Args('password') password: string) {
+    const checkadmin = this.prisma.admin.findFirst({
+      where: { email },
+    });
+    if ((await checkadmin).password === '') {
+      throw new Error('Admin not found');
+    }
+    const isPasswordValid = await bcrypt.compare(
+      password,
+      (await checkadmin).password,
+    );
+    if (!isPasswordValid) {
+      throw new Error('Invalid password');
+    }
 
+    const token = this.jwtService.sign({
+      email: (await checkadmin).email,
+      role: (await checkadmin).role,
+    });
 
+    return { token };
+  }
 
-  
+  @Mutation(() => Admin)
+  async createAdmin(
+    @Args('name') name: string,
+    @Args('email') email: string,
+    @Args('username') username: string,
+    @Args('password') password: string,
+  ) {
+    const saltRounds = parseInt(process.env.BCRYPT_SALT_ROUNDS);
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+    return this.prisma.admin.create({
+      data: { name, email, username, password: hashedPassword },
+    });
+  }
+
+  @Mutation(() => Admin)
+  async UpdateAdmin(
+    @Args('id', { type: () => Int }) id: number,
+    @Args('name') name: string,
+    @Args('email') email: string,
+    @Args('username') username: string,
+  ) {
+    return await this.prisma.admin.update({
+      where: { id },
+      data: { name, email, username },
+    });
+  }
+
+  @Mutation(() => Admin)
+  async DeleteAdmin(@Args('id', { type: () => Int }) id: number) {
+    return await this.prisma.admin.delete({
+      where: { id },
+    });
+  }
 }
